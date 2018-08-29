6c8bfbf2f36b3244a1cef92cb05f52b422d172c9
Nisal Bandara <nisaldbandara96@gmail.com>
2018-08-29 19:24:48 +0530

    Fix inconsitant tabs

--- chmac.py
+++ chmac.py
@@ -12,7 +12,7 @@ def get_args():
 	interface = options.interface
 	mac = options.mac
 	if not (interface or mac):
-		parser.error("[-] Please enter both interface and new MAC Address, use --help for more information.")	
+		parser.error("[-] Please enter both interface and new MAC Address, use --help for more information.")
 	elif not (interface):
 		parser.error("[-] Please enter an interface, use --help for more information.")
 	elif not (mac):
@@ -23,7 +23,7 @@ def get_args():
 
 def ch_mac(interface,new_mac):
 	print("[+] Changing MAC Adreess of " + interface + " to " + new_mac)
-        if check_mac(interface):
+	if check_mac(interface):
 		get_current_mac(interface)
 		subprocess.call(["ifconfig" , interface , "down"])
 		subprocess.call(["ifconfig" , interface , "hw", "ether" , new_mac])
@@ -32,30 +32,28 @@ def ch_mac(interface,new_mac):
 	else:
 		print("[-] The MAC Address can not be read!")
 
-
 def check_mac(interface):
 	ifconfig_result = subprocess.check_output(["ifconfig" , interface])
-        mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , ifconfig_result)
-        if mac_result:
-	        return True
-        else:
-                return False
+	mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , ifconfig_result)
+	if mac_result:
+		return True
+	else:
+		return False
 
 def get_current_mac(interface):
-        ifconfig_result = subprocess.check_output(["ifconfig" , interface])
-        mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , ifconfig_result)
-        if mac_result:
-	        print("[+] Current MAC Address is " + mac_result.group(0))	
+	ifconfig_result = subprocess.check_output(["ifconfig" , interface])
+	mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , ifconfig_result)
+	if mac_result:
+		print("[+] Current MAC Address is " + mac_result.group(0))
 
 def check_new_mac(interface):
 	ifconfig_result = subprocess.check_output(["ifconfig" , interface])
-        mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , ifconfig_result)
-        if mac_result:
-                if  (mac_result.group(0) == options.mac): 
-                        print("[+] The MAC Address of the given interface is successfully changed to " + options.mac)
-                else:
-                        print("[-] The MAC Address could not be changed!")
+	mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , ifconfig_result)
+	if mac_result:
+		if  (mac_result.group(0) == options.mac):
+			print("[+] The MAC Address of the given interface is successfully changed to " + options.mac)
+		else:
+			print("[-] The MAC Address could not be changed!")
 
 options = get_args()
 ch_mac(options.interface , options.mac)
-
